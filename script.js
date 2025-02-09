6fb1a635089f1aa52d3cd2b45f23d497f490e367
git <`GIT_ASKPASS=c:\Users\prash\AppData\Local\Programs\Microsoft VS Code\resources\app\extensions\git\dist\askpass.sh`>
2025-02-09 22:56:41 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 tl.from(".line h1",{
     y : 100,
     opacity: 0,
@@ -25,22 +25,46 @@ tl.from("#line1-part1",{
 });
 
 tl.from(".line h2",{
-    delay:2,
+    delay:1.5,
     opacity:0,
 })
 tl.to("#loader",{
     opacity:0 ,
-    duration:0.3,
-    delay:3,
+    duration:1,
+    delay:0.1,
 });
 
 tl.from("#page1" , {
     delay : 0.2,
     y:1400,
     opacity :0,
-    duration:0.5,
-    ease : Power4
+    duration:1,
+    ease : Power4,
 })
 tl.to("#loader",{
     display:"none",
 })
+
+tl.from(".hero h1 , #hero3 h2",{
+    y:120,
+    duration:0.3,
+    stagger:0.2,
+})
+
+};
+function cursorAnimation(){
+    
+document.addEventListener("mousemove" , function(dets){
+    gsap.to("#crsr" ,{
+      left:dets.x,
+      top:dets.y
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4" , {
+    //Parameters are optional.
+    
+  });
+};
+loadingAnimation();
+cursorAnimation();
